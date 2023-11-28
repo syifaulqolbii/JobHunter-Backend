3f333e70378a4f8e93d09688363eb73dea04a566
Alfridus Elman <alfridusferi@gmail.com>
2023-11-28 13:03:45 +0700

    Backend Edit User Profile

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-const cors = require('cors');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const routes = require("./routes/index");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}/api/v1`);
+  console.log(`Server is running on http://localhost:${port}/api/v1`);
 });
--- migrations/20231107162948-create-user.js
+++ migrations/20231107162948-create-user.js
@@ -1,53 +1,57 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+      },
+      profil: {
+        allowNull: true,
+        type: Sequelize.BLOB,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       address: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       phone: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       role: {
         type: Sequelize.ENUM,
-        values: ['user', 'company'],
-        defaultValue: 'user'
+        values: ["user", "company"],
+        defaultValue: "user",
       },
       about: {
         allowNull: true,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       skill: {
         allowNull: true,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-  }
+    await queryInterface.dropTable("Users");
+  },
 };
