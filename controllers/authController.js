214b393d27777743d944000cfebfc35de81d8296
Attoriq Zalendra Putra <58330985+attoriq-sekuteng@users.noreply.github.com>
2023-12-01 19:18:09 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,7 +39,7 @@ const login = async (req, res) => {
       expiresIn: '1h', 
     });
 
-    res.json({ token });
+    res.json({ token, role: user.role });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
