fdea946231ff8f940274a28c55a5de2657d53e7b
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-12-03 02:21:22 +0700

    minor update

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ const login = async (req, res) => {
     }
 
     const token = jwt.sign({ userId: user.id, role: user.role }, process.env.JWT_SECRET, {
-      expiresIn: '1h', 
+        expiresIn: '1d',
     });
 
     res.json({ token, role: user.role });
